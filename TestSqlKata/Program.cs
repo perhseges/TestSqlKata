51292a66b856e129b2cc0bf130253755b7509f23
Per Hejndorf <perh@seges.dk>
2022-04-10 17:25:44 +0200

    Checked connection-state

--- TestSqlKata/Program.cs
+++ TestSqlKata/Program.cs
@@ -6,50 +6,38 @@ using System.Data.SqlClient;
 //var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
 //var connectionstring = config["ConnectionString"];
 
-var connectionstring = "Server=127.0.0.1;Database=NorthWind;Trusted_Connection=True;";
-
-var compiler = new SqlKata.Compilers.SqlServerCompiler();
-
 // https://github.com/sqlkata/querybuilder/issues/213 Lack of Connection Dispose in documentation
+var connectionstring = "Server=127.0.0.1;Database=NorthWind;Trusted_Connection=True;";
+SqlConnection sqlconnection = new(connectionstring);
+SqlKata.Compilers.SqlServerCompiler compiler = new();
 
-var connection = new SqlConnection(connectionstring);
-var db = new QueryFactory(connection, compiler);
-db.Logger = compiled => { Console.WriteLine(compiled.ToString()); };
+IEnumerable<Customers> records;
+Query query;
 
-var records = await db
-    .Query(nameof(Customers))
-    .Take(5)
-    .OrderByDesc(nameof(Customers.CustomerID))
-    //.Select(ColumnNames<Customers>("CustomerID", nameof(Customers.ContactName)))
-    .Select("CustomerID", "ContactName")
-    .GetAsync<Customers>();
+using (QueryFactory db = new(sqlconnection, compiler))
+{
+    db.Logger = compiled => { Console.WriteLine(compiled.ToString()); };
 
-foreach (var record in records) Console.WriteLine(record.ContactName);
+    records = await db
+        .Query(nameof(Customers))
+        .Take(5)
+        .OrderByDesc(nameof(Customers.CustomerID))
+        //.Select(ColumnNames<Customers>("CustomerID", nameof(Customers.ContactName)))
+        .Select("CustomerID", "ContactName")
+        .GetAsync<Customers>();
+    
+    foreach (var record in records) Console.WriteLine(record.ContactName);
 
-var query = new Query(nameof(Customers)).OrderByDesc(nameof(Customers.CustomerID));
-query.Take(3);
-records = db.Get<Customers>(query);
+    query = new Query(nameof(Customers)).OrderByDesc(nameof(Customers.CustomerID));
+    query.Take(3);
+    records = db.Get<Customers>(query);
 
-//var records = await db.Query<Customers>().Take(5).OrderByDesc(nameof(Customers.CustomerID)).GetAsync<Customers>();
-//var records = await db.Query().FromRaw($"{nameof(Customers)} TABLESAMPLE (5 ROWS)").OrderByDesc(nameof(Customers.CustomerID)).GetAsync<Customers>();
+    foreach (var record in records) Console.WriteLine(record.ContactName);
+}
 
-foreach (var record in records) Console.WriteLine(record.ContactName);
-
+query = new Query(nameof(Customers)).Take(4).OrderByDesc(nameof(Customers.CustomerID));
 var compiledquery = compiler.Compile(query);
 Console.WriteLine(compiledquery.Sql);
-
-using (SqlConnection sqlcon = new(connectionstring))
-{
-    SqlCommand cmd = new SqlCommand(compiledquery.Sql, sqlcon);
-    var sqlparameters = compiledquery.NamedBindings.Select(p => new SqlParameter(p.Key, p.Value)).ToArray();
-    cmd.Parameters.AddRange(sqlparameters);
-
-    sqlcon.Open();
-    var reader = cmd.ExecuteReader();
-    while (reader.Read())
-        Console.WriteLine(reader[5]);
-    sqlcon.Close();
-};
 
 using (SqlConnection dappercon = new(connectionstring))
 {
@@ -57,11 +45,25 @@ using (SqlConnection dappercon = new(connectionstring))
     records = await dappercon.QueryAsync<Customers>(compiledquery.Sql, compiledquery.NamedBindings);
 }
 
+using (SqlConnection sqlcon = new(connectionstring))
+{
+    SqlCommand cmd = new SqlCommand(compiledquery.Sql, sqlcon);
+    var sqlparameters = compiledquery.NamedBindings.Select(p => new SqlParameter(p.Key, p.Value)).ToArray();
+    cmd.Parameters.AddRange(sqlparameters);
+    sqlcon.Open();
+
+    var reader = cmd.ExecuteReader();
+    while (reader.Read())
+
+        Console.WriteLine(reader[5]);
+    sqlcon.Close();
+};
+
 Console.WriteLine("Finished..");
 
 // -------------------
 
-string[] ColumnNames<T> (params string[] nameof)
+string[] ColumnNames<T>(params string[] nameof)
 {
     return nameof.Select(s => $"{typeof(T).Name}.{s}").ToArray();
 }
