bda843cd7e3146a4a7e6452b86e805bae0fdec16
Per Hejndorf <perh@seges.dk>
2022-04-10 08:05:24 +0200

    Add project files.

---
+++ TestSqlKata/GzipExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System.IO.Compression;
+
+public static class GzipExtensions
+{
+    /// <summary>
+    /// Gzip from a stream to another stream
+    /// </summary>
+    /// <param name="inStream"></param>
+    /// <param name="outStream"></param>
+    /// <param name="gzipMode"></param>
+    public static void GZipStream(this Stream inStream, Stream outStream, CompressionMode gzipMode = CompressionMode.Compress)
+    {
+        if (gzipMode == CompressionMode.Compress)
+        {
+            using (var compressionStream = new GZipStream(outStream, CompressionMode.Compress))
+            {
+                inStream.CopyTo(compressionStream);
+            }
+        }
+        else
+        {
+            using (var compressionStream = new GZipStream(inStream, CompressionMode.Decompress))
+            {
+                compressionStream.CopyTo(outStream);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Gzip a byte array to a new gzipped byte array
+    /// /// </summary>
+    /// <param name="inBytes"></param>
+    /// <param name="gzipMode"></param>
+    /// <returns>Byte array</returns>
+
+    public static byte[] GZipBytes(this byte[] inBytes, CompressionMode gzipMode = CompressionMode.Compress)
+    {
+        using (var msi = new MemoryStream(inBytes))
+        {
+            using (var mso = new MemoryStream())
+            {
+                msi.GZipStream(mso, gzipMode);
+                return mso.ToArray();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Gzip a stream to a new gzipped byte array
+    /// /// </summary>
+    /// <param name="inBytes"></param>
+    /// <param name="gzipMode"></param>
+    /// <returns>Byte array</returns>
+
+    public static byte[] GZipBytes(this Stream inStream, CompressionMode gzipMode = CompressionMode.Compress)
+    {
+        inStream.Position = 0;
+        using (var mso = new MemoryStream())
+        {
+            inStream.GZipStream(mso, gzipMode);
+            return mso.ToArray();
+        }
+    }
+
+}
---
+++ TestSqlKata/Program.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+
+using SqlKata.Execution;
+using System.Data.SqlClient;
+using System.IO.Compression;
+using System.Text.Json;
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+var connectionstring = config["ConnectionString"];
+
+
+var connection = new SqlConnection(config["ConnectionString"]);
+var compiler = new SqlKata.Compilers.SqlServerCompiler();
+var db = new QueryFactory(connection, compiler);
+
+var a = await db.Query("Audit").Take(5).OrderByDesc("Id").GetAsync<Audit>();
+
+var b = await db.Query("Audit").Take(7).OrderByDesc("Id").GetAsync<Audit>();
+
+//var query = new Query("Audit").Select("Id"). OrderByDesc("Id").Take(5);
+//SqlResult result = compiler.Compile(query);
+//string sql = result.Sql;
+
+
+//using (var con = new SqlConnection(connectionstring))
+//{
+//    con.Open();
+
+//    // Dapper 
+//    Console.WriteLine("Fetch data");
+
+//    var audits = await con.QueryAsync<Audit>("SELECT TOP 20 * FROM Audit ORDER BY Id DESC;");
+
+//    foreach (var audit in audits)
+//    {
+//        using (var memorystream = new MemoryStream())
+//        {
+//            // Data-object (byte array) to transmit
+//            await JsonSerializer.SerializeAsync(memorystream, audit);
+//            var compressedbytes = memorystream.GZipBytes(CompressionMode.Compress);
+            
+//            // Print
+//            var percent = compressedbytes.Length * 100 / memorystream.Length;
+//            Console.WriteLine($"{audit.Updatetime} {memorystream.Length,10:#} {compressedbytes.Length,10:#} {percent,4:#}%");
+
+//            // Unpack received data
+//            var decompressedbytes = compressedbytes.GZipBytes(CompressionMode.Decompress);
+//            var x = JsonSerializer.Deserialize<Audit>(decompressedbytes);
+//        }
+//    }
+//}
+
+Console.WriteLine("Finished..");
